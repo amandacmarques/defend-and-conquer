617edc60685532b4039d4df838b82f74d711b456
Andrew Seaman <andrew.seaman@mullenmail.com>
2020-04-16 18:39:53 -0700

    Add todo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Title from './Components/Title'
 import logo from './logo.svg';
 import './App.css';
+import TodoComponent from './Components/TodoComponent'
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <TodoComponent />
     </div>
   );
 }
