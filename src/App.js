c93e5960eb5b1c7b34dbb21addc74579c3159100
Amanda Marques <amarqu21@lion.lmu.edu>
2020-04-16 18:46:31 -0700

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Title from './Components/Title'
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
