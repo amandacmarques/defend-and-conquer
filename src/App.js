d3cab630b53039165f40f55a21b30f61b8e140a6
Amanda Marques <amarqu21@lion.lmu.edu>
2020-04-23 18:49:01 -0700

    Make grid formatting work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Title from './Components/Title'
 import './App.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-import TodoComponent from './Components/TodoComponent'
+import TodoComponent from './Components/TodoComponent';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Title />
-      <TodoComponent />
+      <Container>
+        <Row className="justify-content-md-center">
+        <Title />
+        </Row>
+        <Row>
+          <Col><TodoComponent /></Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
